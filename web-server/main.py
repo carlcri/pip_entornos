b4e4cf688c462d4fd8c5a81e49e27f13ebb91750
julianC <mantillajulian234@gmail.com>
2023-10-03 19:53:42 -0500

    termino fastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,9 +1,28 @@
 from store import get_categories, get_random_categorie
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get('/') 
+def index():
+    return 'hola mundo platzi'
+
+@app.get('/contact', response_class=HTMLResponse)
+def contact():
+    return """
+        <h1>Hola soy un endpoint</h1>
+        <p>soy un parrafo</p1>
+"""
+
+
+'''
 def run():
     get_categories()
     get_random_categorie()
 
 if __name__ == '__main__':
     run()
+'''
+
 
